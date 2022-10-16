56997841e74f8b9be491fd19d86d34feb855a88e
Zorko Aleksandr <alexzorko@yandex.ru>
2022-10-17 00:49:15 +0200

    Добавлен файл editconfig. Код отформатирован в соотсвествии с этими правилами.

--- funcions.php
+++ funcions.php
@@ -1,13 +1,21 @@
 <?php
 
-function printMovie($movie, int $movieNumber):void{
-	echo "{$movieNumber}. ".$movie["title"]." ({$movie["release_year"]}), "."{$movie["age_restriction"]}+."." Rating - {$movie["rating"]}\n";
+function printMovie($movie, int $movieNumber): void
+{
+	echo "{$movieNumber}. "
+		. $movie["title"]
+		. " ({$movie["release_year"]}), "
+		. "{$movie["age_restriction"]}+."
+		. " Rating - {$movie["rating"]}\n";
 }
 
-function getAvailableMovies(array $movies, float $age): void{
+function getAvailableMovies(array $movies, float $age): void
+{
 	$countAvailableMovies = 0;
-	foreach ($movies as $film) {
-		if ($film["age_restriction"]<=$age){
+	foreach ($movies as $film)
+	{
+		if ($film["age_restriction"] <= $age)
+		{
 			$countAvailableMovies++;
 			printMovie($film, $countAvailableMovies);
 		}
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 require "funcions.php";
 require "movies.php";
-/**@var array $movies*/
+/**@var array $movies */
 
-$age=readline("Enter age: ");
-if (is_numeric($age)) getAvailableMovies($movies, floor((float)$age));
-else echo "[ERROR]: Age must be numeric!";
+$age = readline("Enter age: ");
+if (is_numeric($age))
+{
+	getAvailableMovies($movies, floor((float)$age));
+}
+else
+{
+	echo "[ERROR]: Age must be numeric!";
+}
