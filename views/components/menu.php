945988474f855b9ace667d00c36d76aba3d6fa20
Zorko Aleksandr <alexzorko@yandex.ru>
2022-11-29 14:51:38 +0200

    Пункт меню, в котором сейчас находится пользователь теперь подсвечивается

--- views/components/menu.php
+++ views/components/menu.php
@@ -2,22 +2,22 @@
 /**
  * @var array $genres
  */
+
+$currentPage=$_SERVER['REQUEST_URI'];
 ?>
 
 <ul class="menu">
-	<li class="menu-item">
+	<li class="menu-item <?= ($currentPage === "/public/index.php") ? 'menu-item-active' : ''?>">
 		<a href="/public/index.php"><?= getConfigurationOption("HOME_PAGE") ?></a>
 	</li>
 
-	<?php
-	foreach ($genres as $key => $genre): ?>
-		<li class="menu-item">
+	<?php foreach ($genres as $key => $genre): ?>
+		<li class="menu-item <?= ($currentPage === "/public/index.php?genre=$key") ? 'menu-item-active' : ''?>">
 			<a href="/public/index.php?genre=<?= $key ?>"><?= $genre ?></a>
 		</li>
-	<?php
-	endforeach ?>
+	<?php endforeach ?>
 
-	<li class="menu-item">
+	<li class="menu-item <?= ($currentPage === "/public/favorite.php") ? 'menu-item-active' : ''?>">
 		<a href="/public/favorite.php"><?= getConfigurationOption("FAVORITES_PAGE") ?></a>
 	</li>
 </ul>
